47245b1f8751268df2088fc08a7a37fcf95dce2c
Niladri Ghosh <niladri1406@gmail.com>
2021-09-15 17:02:18 +0530

    feat : add element in sorted array

---
+++ 03.Array ADT/08_add_element_in_sorted_list.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Array{
+    public:
+        int *A;
+        int size;
+        int length;
+};
+
+void AddElement(Array *arr, int element){
+    int i = arr->length-1;
+    while(arr->A[i]>element){
+        arr->A[i+1] = arr->A[i];
+        i--;
+    }
+    arr->A[i+1]=element;
+}
+
+void Display(Array arr){
+    for(int i=0; i<arr.length; i++){
+        cout<<arr.A[i]<<endl;
+    }
+}
+
+int main(){
+    Array arr;
+    cout<<"Enter the size of array "<<endl;
+    cin>>arr.size;
+    arr.A = new int[arr.size];
+    cout<<"Enter the length of array "<<endl;
+    cin>>arr.length;
+    cout<<"Enter the elements in the array : "<<endl;
+    for(int i=0; i<arr.length; i++){
+        cin>>arr.A[i];
+    }
+
+    AddElement(&arr,34);
+
+    Display(arr);
+
+    return 0;
+}
