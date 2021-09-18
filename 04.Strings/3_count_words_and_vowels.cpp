5bb6549fef816fd1c05b75d538fc918e746a6620
Niladri Ghosh <niladri1406@gmail.com>
2021-09-18 22:51:32 +0530

    feat : count words and vowels in string

---
+++ 04.Strings/3_count_words_and_vowels.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int wordCount(char *s){
+    int count = 1;
+    for(int i=0; s[i]!='\0'; i++){
+        if(s[i]==32)
+            count++;
+    }
+    return count;
+}
+
+int vowelCount(char *s){
+    int count = 0;
+    for(int i=0; s[i]!='\0'; i++){
+        if(s[i]==65 || s[i]==97 || s[i]==69 || s[i]==101 || s[i]==73 
+            || s[i]==105 || s[i]==79 || s[i]==111 || s[i]==85 || s[i]==117)
+                count++;
+    }
+    return count;
+}
+
+int main(){
+    char s[] = "Hello! Welcome to the Jungle.";
+    cout<<"Word Count  : "<<wordCount(s)<<endl;
+    cout<<"Vowel Count : "<<vowelCount(s)<<endl;
+    return 0;
+}
